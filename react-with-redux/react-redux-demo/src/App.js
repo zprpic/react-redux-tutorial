18e6f6780233a98e383c1ffce4999d94652d5fc0
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-09-19 12:43:10 +0200

    connecting react with redux via react-redux. basics

---
+++ react-with-redux/react-redux-demo/src/App.js
@@ -0,0 +1,11 @@
+import CakeContainer from "./Components/CakeContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <CakeContainer />
+    </div>
+  );
+}
+
+export default App;
