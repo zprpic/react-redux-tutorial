65c0724eb29c047ac4f36e53d5f8daca8e646ee5
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-09-19 11:26:53 +0200

    Redux middleware - logger

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,5 +1,7 @@
 const redux = require("redux");
-const { createStore, combineReducers } = redux;
+const reduxLogger = require("redux-logger");
+const { createStore, combineReducers, applyMiddleware } = redux;
+const logger = reduxLogger.createLogger();
 
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
@@ -61,11 +63,9 @@ const rootReducer = combineReducers({
   iceCream: iceCreamReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log("Initial state:", store.getState());
-const unsubscribe = store.subscribe(() =>
-  console.log("updated state", store.getState())
-);
+const unsubscribe = store.subscribe(() => {});
 
 store.dispatch(buyCake());
 store.dispatch(buyCake());
