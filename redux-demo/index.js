2dc27c45e472721a1f5e535c34bcfcab1b444cf2
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-09-19 10:58:59 +0200

    React-redux combo basics...

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,1 +1,41 @@
-console.log("from index.js");
+const redux = require("redux");
+const { createStore } = redux;
+
+const BUY_CAKE = "BUY_CAKE";
+
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    description: "first redux action",
+  };
+}
+
+// (prevState, action) => newState
+
+const initialState = {
+  numberOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numberOfCakes: state.numberOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log("Initial state:", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("updated state", store.getState())
+);
+
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+unsubscribe();
