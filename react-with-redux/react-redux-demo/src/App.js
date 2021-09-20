67f44035ae6a4b6716f7afcb99b55639e157af7e
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-09-20 10:12:49 +0200

    ownProps...

--- react-with-redux/react-redux-demo/src/App.js
+++ react-with-redux/react-redux-demo/src/App.js
@@ -5,14 +5,17 @@ import HooksCakeContainer from "./Components/HooksCakeContainer";
 import IceCreamContainer from "./Components/IceCreamContainer";
 import HooksChocolateContainer from "./Components/HooksChocolateContainer";
 import NewCakeContainer from "./Components/NewCakeContainer";
+import ItemContainer from "./Components/ItemContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
         <HooksCakeContainer />
         <CakeContainer />
         <NewCakeContainer />
+        <ItemContainer iceCream />
         <IceCreamContainer />
         <HooksChocolateContainer />
       </div>
